97abb9815236d89781687d11682e2fb21c4072ab
Harshit Joshi <Harshitrocky9@gmail.com>
2023-12-20 16:01:56 +0530

    Assignment uploaded

---
+++ assignmentwork/src/App.jsx
@@ -0,0 +1,18 @@
+import Highlights from "./components/assignment1/Highlights"
+import { CourseSection } from "./components/assignment2/CourseSection"
+import CareerScope from "./components/assignment3/CareerScope"
+import IndiaVsAbroad from "./components/assignment4/IndiaVsAbroad"
+
+function App() {
+
+  return (
+    <>
+      <Highlights />
+      <CourseSection />
+      <CareerScope />
+      <IndiaVsAbroad />
+    </>
+  )
+}
+
+export default App
